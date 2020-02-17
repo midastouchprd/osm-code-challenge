b8220bcc5f7d5ac715d8eac6c73e81035d5307a2
Oluwatosin Awofeso <midastouchproductions@gmail.com>
2020-02-17 00:40:00 -0600

    connect to db

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+// Packages
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 9050;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () => console.log(`Server Running on ${port}!`));
+// DB Connection
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection
+  .on("error", console.error.bind(console, "connection error:"))
+  .on("disconnected", () => {
+    console.log("db disconnected");
+  })
+  .on("open", () => {
+    console.log("db connected");
+    app.listen(port, () => console.log(`Server Running on ${port}!`));
+  });

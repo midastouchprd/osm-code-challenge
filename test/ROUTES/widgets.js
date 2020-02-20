dcd74bef1cb74252fa5ed7b3665a4cbb0303c617
Tosin Awofeso <midastouchproductions@gmail.com>
2020-02-20 05:04:45 -0600

    input multiples while still not dupicating keys (#4)

--- test/ROUTES/widgets.js
+++ test/ROUTES/widgets.js
@@ -7,6 +7,15 @@ const mongoose = require("mongoose");
 const db = require("../../mongoose");
 const Widget = require("../../models/Widget");
 
+const {
+  fakeWidgets: {
+    downPinkTriangle,
+    downRedCircle,
+    upPinkTriangle,
+    strangeBottomYellowSquare
+  }
+} = mockData;
+
 describe("WIDGET ROUTE TESTING", function() {
   before(async function() {
     await Widget.deleteMany({});
@@ -17,18 +26,12 @@ describe("WIDGET ROUTE TESTING", function() {
       this.timeout(15000);
       request(app)
         .post("/widgets")
-        .send(mockData.fakeWidgets.downRedCircle)
+        .send(downRedCircle)
         .end(function(err, res) {
           expect(res.statusCode).to.equal(201);
-          expect(res.body.data.color).to.eql(
-            mockData.fakeWidgets.downRedCircle.color
-          );
-          expect(res.body.data.qualities).to.eql(
-            mockData.fakeWidgets.downRedCircle.qualities
-          );
-          expect(res.body.data.shape).to.eql(
-            mockData.fakeWidgets.downRedCircle.shape
-          );
+          expect(res.body.data[0].color).to.eql(downRedCircle.color);
+          expect(res.body.data[0].qualities).to.eql(downRedCircle.qualities);
+          expect(res.body.data[0].shape).to.eql(downRedCircle.shape);
           done();
         });
     });
@@ -36,60 +39,62 @@ describe("WIDGET ROUTE TESTING", function() {
       this.timeout(15000);
       request(app)
         .post("/widgets")
-        .send(mockData.fakeWidgets.downRedCircle)
+        .send(downRedCircle)
         .end(function(err, res) {
           expect(res.statusCode).to.equal(403);
           done();
         });
     });
   });
-  describe("PUT /widgets/:id", function() {
-    it("should update a widget", function(done) {
-      this.timeout(30000);
-      // make a new widget and save the id
-      request(app)
-        .post("/widgets")
-        .send(mockData.fakeWidgets.strangeBottomYellowSquare)
-        .end(function(err, res) {
-          expect(res.statusCode).to.equal(201);
-          expect(res.body.data.color).to.eql(
-            mockData.fakeWidgets.strangeBottomYellowSquare.color
-          );
-          expect(res.body.data.qualities).to.eql(
-            mockData.fakeWidgets.strangeBottomYellowSquare.qualities
-          );
-          expect(res.body.data.shape).to.eql(
-            mockData.fakeWidgets.strangeBottomYellowSquare.shape
-          );
-          request(app)
-            .put(`/widgets/${res.body.data._id}`)
-            .send(mockData.fakeWidgets.upPinkTriangle)
-            .end(function(err, res2) {
-              expect(res2.statusCode).to.equal(201);
-              expect(res2.body.data.color).to.eql(
-                mockData.fakeWidgets.upPinkTriangle.color
-              );
-              expect(res2.body.data.qualities).to.eql(
-                mockData.fakeWidgets.upPinkTriangle.qualities
-              );
-              expect(res2.body.data.shape).to.eql(
-                mockData.fakeWidgets.upPinkTriangle.shape
-              );
-              done();
-            });
-        });
-    });
+  it("should create multiple widgets", function(done) {
+    this.timeout(15000);
+    request(app)
+      .post("/widgets")
+      .send([downPinkTriangle, upPinkTriangle])
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(201);
+        expect(res.body.data).to.be.an("array");
+        expect(res.body.data.length).to.equal(2);
+        done();
+      });
   });
+});
+describe("PUT /widgets/:id", function() {
+  it("should update a widget", function(done) {
+    this.timeout(30000);
+    // make a new widget and save the id
+    request(app)
+      .post("/widgets")
+      .send(strangeBottomYellowSquare)
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(201);
+        expect(res.body.data[0].color).to.eql(strangeBottomYellowSquare.color);
+        expect(res.body.data[0].qualities).to.eql(
+          strangeBottomYellowSquare.qualities
+        );
+        expect(res.body.data[0].shape).to.eql(strangeBottomYellowSquare.shape);
+        request(app)
+          .put(`/widgets/${res.body.data[0]._id}`)
+          .send(upPinkTriangle)
+          .end(function(err, res2) {
+            expect(res2.statusCode).to.equal(201);
+            expect(res2.body.data.color).to.eql(upPinkTriangle.color);
+            expect(res2.body.data.qualities).to.eql(upPinkTriangle.qualities);
+            expect(res2.body.data.shape).to.eql(upPinkTriangle.shape);
+            done();
+          });
+      });
+  });
+});
 
-  describe("GET /widgets", function() {
-    it("should return all widgets", function(done) {
-      request(app)
-        .get("/widgets")
-        .end(function(err, res) {
-          expect(res.statusCode).to.equal(200);
-          expect(res.body.data).to.be.an("array");
-          done();
-        });
-    });
+describe("GET /widgets", function() {
+  it("should return all widgets", function(done) {
+    request(app)
+      .get("/widgets")
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.data).to.be.an("array");
+        done();
+      });
   });
 });

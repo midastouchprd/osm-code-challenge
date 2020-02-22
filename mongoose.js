df8cb7f2eb94aa99bfcfb887a5db7c0137e6ebe2
Oluwatosin Awofeso <midastouchproductions@gmail.com>
2020-02-22 03:43:34 -0600

    notes

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,10 @@
 // DB Connection
 const mongoose = require("mongoose");
 let uri;
+
+//Run in production mode to use the live mongo instance.
+
+//otherwise you need a database named test
 if (process.env.NODE_ENV !== "production") {
   uri = "mongodb://localhost";
 } else {
